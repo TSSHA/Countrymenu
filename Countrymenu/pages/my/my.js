0a9d61737e72fd11abe4546535546819d217b912
michael <75024156+TSSHA@users.noreply.github.com>
2022-05-19 16:53:08 +0800

    获取用户头像和昵称

--- Countrymenu/pages/my/my.js
+++ Countrymenu/pages/my/my.js
@@ -1,44 +1,52 @@
-const app = getApp();
 Page({
-    data:{
-      text:'未登录',
-      isLogin: false,
-      img: "../../img/maoyan.png",
-    },
-    onLoad(){
-       
-      this.setData({
-        text:wx.getStorageSync('nickName'),
-        img:wx.getStorageSync('img')
-      });
-      if(wx.getStorageSync('nickName')){
-        this.data.isLogin = true;
-      }
-    },
-    logins(e){
-      if(this.data.isLogin) return false;
-      wx.getUserProfile({
-        desc: '用于完善会员资源',
-        success:(res=>{
-           wx.setStorageSync('img',res.userInfo.avatarUrl)
-          let {encryptedData,iv,userInfo} =res;
-          wx.login({
-            success:(res)=>{
-              let {code} =res;
-              let param = {
-                encryptedData,iv,userInfo,code
-              }
-              app.post('/user/login',param).then(res=>{
-                this.setData({
-                  text:res.data.nickName
-                });
-                wx.setStorageSync('nickName',res.data.nickName)
-              }).catch(err=>{
-                console.log(err);
-              })
-            }
-          })
+  data:{
+    userInfo: {}
+  },
+  onLoad:function(e){
+    this.setUserInfoStorageTime();
+  },
+  getUserProfile() {
+    var that = this;
+    wx.showModal({
+      title: "提示",
+      content: "是否允许获取微信昵称和头像？",
+      success(res) {
+        if (res.confirm) {
+          wx.getUserProfile({
+            desc: "用于完善用户资料", // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+            success: (res) => {
+              that.setData({
+                userInfo: res.userInfo
+              });
+              wx.setStorageSync("userInfo", res.userInfo);
+              
+              let setNowTime = Date.now() + 3600 * 1000 * 24 * 30;  // 设置了30天有效期
+              wx.setStorageSync("userInfoStorageTime", setNowTime);
+            },
+            fail: function (err) {
+              console.log(err);
+            },
+          });
+        }
+      },
+    });
+  },
+  setUserInfoStorageTime() {
+    var that = this;
+    let nowTime = Date.now();
+    let oldTime = wx.getStorageSync("userInfoStorageTime");
+    let userInfo = wx.getStorageSync("userInfo");
+    if ( userInfo.nickName != undefined && userInfo.nickName != null && userInfo.nickName != "" ) {
+      if (oldTime && nowTime < oldTime) {
+        that.setData({
+          userInfo:userInfo
         })
-      })
+        return;
+      } else {
+        that.getUserProfile();
+      }
+    } else {
+      that.getUserProfile();
     }
+  },
 })
