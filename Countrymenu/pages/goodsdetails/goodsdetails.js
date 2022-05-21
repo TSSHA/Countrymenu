c88dc0520f8fe10b1b53ef6f296d4ce907f6eec0
michael <75024156+TSSHA@users.noreply.github.com>
2022-05-21 15:21:15 +0800

    页面跳传菜谱类，商品类，菜谱，商品id传参

--- Countrymenu/pages/goodsdetails/goodsdetails.js
+++ Countrymenu/pages/goodsdetails/goodsdetails.js
@@ -13,7 +13,8 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (data) {
+    console.log(data);
     let newArray4 = this.data.forthData;
     this.setData({
       goodsData:newArray4
